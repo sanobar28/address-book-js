b4f964d486a5aa4b860f7d18584dc6fcf07b6cb0
sanobar28 <sanobar.mujawar@gmail.com>
2021-07-13 22:46:40 +0530

    added: update contact detail by name

--- AddressBook.js
+++ AddressBook.js
@@ -142,10 +142,18 @@ try {
     AddressBookArray.push(new AddressBook("Payal", "Giri", "Kothrud", "Pune", "Maharashtra", "413007",
         "8878368999", "payalgiri@gmail.com"));
 
-    AddressBookArray.push(new AddressBook("Soham", "deshmukh", "navipeth", "Banglore", "Karnataka", "411007",
+    AddressBookArray.push(new AddressBook("Soham", "Deshmukh", "navipeth", "Banglore", "Karnataka", "411007",
         "7090355988", "sohamdeshmukh@gmail.com"));
         
-    AddressBookArray.forEach((contact) => console.log(contact.toString()));
+   //finding Contact using name
+   let check = AddressBookArray.findIndex(contact => contact.firstName == "Neha");
+   //updating the contact detail
+   AddressBookArray[check].zip = "412005";
+
+   //display updated contacts   
+   console.log(" updated Contact Details");
+   AddressBookArray.forEach((contact) => console.log(contact.toString()));
+   
 } catch (e) {
     console.log(e);
 }
